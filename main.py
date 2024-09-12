98ba3a577ec701ed2046691a16cf276c1c1ecb8b
bkandh30 <kandhari.bhavya20@gmail.com>
2024-09-11 19:04:07 -0700

    respond with body

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def parse_request(request_data):
     return method, path, version
 
 def get_response(path):
-    responses = {
-        "/": "HTTP/1.1 200 OK\r\n\r\n"
-    }
-    default_response = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-    return responses.get(path, default_response)
+    if path == "/":
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    elif path.startswith("/echo/"):
+        value = path.split("/echo/")[1]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(value)}\r\n\r\n{value}".encode()
+    else:
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+    return response
 
 
 def handle_request(client_socket):
